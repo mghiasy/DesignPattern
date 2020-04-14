89e4fce8e1e4abf95370f2ee7e6735d96b2a3d7a
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-14 18:14:29 -0500

    Modify spring framework__Dependency injection with Java Config + classpath scanning + autowiring

--- SpringFramework/SimpleSpring_D/src/main/java/customers/AppConfig.java
+++ SpringFramework/SimpleSpring_D/src/main/java/customers/AppConfig.java
@@ -1,15 +1,14 @@
 package customers;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+//Dependency injection with Java Config + classpath scanning + autowiring
+//We dont have any XML file
+//there is a configuration class --> but there is nothing in it
 @Configuration
+//class path scanning
 @ComponentScan
 public class AppConfig {
-
-    public ICustomerService icustomerService(){
-        return new CustomerService();
-    }
 
 }
--- SpringFramework/SimpleSpring_D/src/main/java/customers/Application.java
+++ SpringFramework/SimpleSpring_D/src/main/java/customers/Application.java
@@ -1,12 +1,11 @@
 package customers;
 
-import configuration.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
+		//context based on AppConfig class and Annotations
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		ICustomerService customerService = context.getBean("customerService",
 				ICustomerService.class);
