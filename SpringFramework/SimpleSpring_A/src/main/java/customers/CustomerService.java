ab7a4a4a020e871090bc87bdf2a15548e4628a4f
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-13 23:12:54 -0500

    Add spring framework__>  setter injection

---
+++ SpringFramework/SimpleSpring_A/src/main/java/customers/CustomerService.java
@@ -0,0 +1,23 @@
+package customers;
+
+public class CustomerService implements ICustomerService {
+	ICustomerDAO customerDAO ;//= new CustomerDAO();
+	IEmailSender emailSender ;//= new EmailSender();
+
+	public void addCustomer(String name, String email, String street,
+			String city, String zip) {
+		Customer customer = new Customer(name, email);
+		Address address = new Address(street, city, zip);
+		customer.setAddress(address);
+		customerDAO.save(customer);
+		emailSender.sendEmail(email, "Welcome " + name + " as a new customer");
+	}
+
+    public void setCustomerDAO(CustomerDAO customerDAO) {
+		this.customerDAO=customerDAO;
+    }
+
+	public void setEmailSender(EmailSender emailSender) {
+		this.emailSender=emailSender;
+	}
+}
